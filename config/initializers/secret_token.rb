8cea35796e007e5ca0a185c8ee85eff7eecc570e
Sasha Klein <sashafklein@gmail.com>
2014-03-04 11:58:16 -0800

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+StripeTest::Application.config.secret_token = '60ea5a659b07ab5147a70a3291cfbbcaa5eab89c7f3e6e005749f7b677dd28a856c8efdcf90f4388f216fd3957e72154b6cd359de81f212e721f6186b3c26467'
